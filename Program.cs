d5b5e6819aaf15def9c9782c19968ffd3d12004e
RajdeepSonawane <rajdeepttdl@outlook.com>
2025-06-08 23:24:47 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://ttdattendanceportalprod-frontend.onrender.com/api")
+        policy.WithOrigins("https://ttdattendanceportalprod-frontend.onrender.com")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
